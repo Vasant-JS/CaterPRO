5e872a01947ae62cdaf3c2bc885cb82696020804
Vasant-JS <vasant007.vk@gmail.com>
2026-09-05 16:58:19 +0530

    Show synced historical events by default

--- frontend/lib/src/events.dart
+++ frontend/lib/src/events.dart
@@ -30,7 +30,7 @@ class _EventsScreenState extends State<EventsScreen> {
   String? clientFilter;
   String? dateFilter;
   DateTimeRange? dateRangeFilter;
-  bool showPastEvents = false;
+  bool showPastEvents = true;
   bool showOverduePayments = false;
   String paymentFilter = 'All';
   static const shortMonths = [
@@ -280,7 +280,7 @@ class _EventsScreenState extends State<EventsScreen> {
     clientFilter = null;
     dateFilter = null;
     dateRangeFilter = null;
-    showPastEvents = false;
+    showPastEvents = true;
     showOverduePayments = false;
     paymentFilter = 'All';
     query = '';
